9973d97e5488eb803a2116b644356a77bf47490b
Jack Steam <jacksteamdev@gmail.com>
2020-02-14 11:42:32 -0600

    Refactor plopfile into index

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,13 @@
 import { NodePlopAPI } from 'plop'
 import autoDiscoverGenerators from './autoDiscoverGenerators'
 
+const { compilerOptions } = require('../tsconfig.json')
+
+require('ts-node').register({
+  compilerOptions,
+  transpileOnly: true,
+})
+
 export default function(plop: NodePlopAPI) {
   autoDiscoverGenerators(plop)
 }
