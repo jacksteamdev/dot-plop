a992a45a97d246c2edae04e932b8cd9127379b9b
Jack Steam <teamsteamdev@gmail.com>
2020-03-09 15:45:31 -0500

    Refactor auto discovery

--- src/autoDiscover.ts
+++ src/autoDiscover.ts
@@ -1,24 +1,15 @@
 import { existsSync, readdirSync } from 'fs-extra'
 import importCwd from 'import-cwd'
 import { join } from 'path'
-import { NodePlopAPI, PlopGenerator, CustomActionFunction } from 'plop'
-
-const dotPlopPath = join(process.cwd(), '.plop')
-
-const paths = {
-  dotPlop: dotPlopPath,
-  generators: join(dotPlopPath, 'generators'),
-  helpers: join(dotPlopPath, 'helpers'),
-  actions: join(dotPlopPath, 'actions'),
-}
+import * as paths from './paths'
+import { CustomActionFunction, HandlebarsHelper, InquirerPrompt, NodePlopAPI, PlopGenerator } from './types'
 
 function loadModulesInDir<T>(dirPath: string): [string, T][] {
   return existsSync(dirPath)
     ? readdirSync(dirPath).flatMap((p) => {
-        const { excluded = [], ...elements } = importCwd(join(dirPath, p)) as Record<
-          string,
-          T
-        > & {
+        const { excluded = [], ...elements } = importCwd(
+          join(dirPath, p),
+        ) as Record<string, T> & {
           excluded: string[]
         }
         delete elements.default
@@ -30,7 +21,7 @@ function loadModulesInDir<T>(dirPath: string): [string, T][] {
     : []
 }
 
-export function autoDiscover(plop: NodePlopAPI) {
+export function autoDiscover(plop: NodePlopAPI): void {
   if (!existsSync(paths.dotPlop))
     throw new Error(`could not find ${paths.dotPlop}`)
 
@@ -38,7 +29,7 @@ export function autoDiscover(plop: NodePlopAPI) {
 
   loadModulesInDir<PlopGenerator>(paths.generators).forEach(
     ([key, generator]) => {
-      plop.setGenerator(key, generator)
+      plop.setGenerator(key, callIfFn(generator))
     },
   )
 
@@ -46,15 +37,29 @@ export function autoDiscover(plop: NodePlopAPI) {
 
   loadModulesInDir<CustomActionFunction>(paths.actions).forEach(
     ([key, actionFn]) => {
-      plop.setActionType(key, actionFn)
+      plop.setActionType(key, callIfFn(actionFn))
     },
   )
 
   /* ----------------- LOAD HANDLEBARS HELPERS ----------------- */
 
-  loadModulesInDir<Handlebars.HelperDelegate>(paths.helpers).forEach(
-    ([key, helper]) => {
-      plop.setHelper(key, helper)
-    },
-  )
+  loadModulesInDir<HandlebarsHelper>(paths.helpers).forEach(([key, helper]) => {
+    plop.setHelper(key, callIfFn(helper))
+  })
+
+  /* ------------------- LOAD INQUIRER PROMPT ------------------ */
+
+  loadModulesInDir<InquirerPrompt>(paths.prompts).forEach(([key, prompt]) => {
+    plop.setPrompt(key, callIfFn(prompt))
+  })
+
+  /* ------------------------ FUNCTIONS ------------------------ */
+
+  function callIfFn(value: any) {
+    if (typeof value === 'function') {
+      return value(plop)
+    } else {
+      return value
+    }
+  }
 }
